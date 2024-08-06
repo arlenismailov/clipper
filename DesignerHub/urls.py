707f83e9b2fc32962c1d414bc93c9064bc3f4c86
arlen <arlenismailov02@gmail.com>
2024-08-06 12:06:43 +0600

    allauth

--- DesignerHub/urls.py
+++ DesignerHub/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('DesignerHubApp.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
